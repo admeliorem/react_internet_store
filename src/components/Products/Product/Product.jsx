79c5d9d2193954379c6ee9e774d875b0984828e2
admeliorem <vinnipuh545@gmail.com>
2021-05-17 23:34:01 +0300

    change folders directory

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card, CardMedia,  CardActions, Typography, IconBotton} from '@material-ui/core' 
+import { AddShoppingCart } from '@material-ui/ixons' 
+
+const Product = () => {
+    return (
+        <Card>
+            git 
+        </Card>
+    )
+}
+
+export default Product
